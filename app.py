b1811e2e1f2167f2102f6edf9f9544c90256ce73
AndersHjemDK <af@hjem.dk>
2025-12-20 18:00:38 +0100

    extra.

--- app.py
+++ app.py
@@ -1,65 +1,5 @@
-import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-
-# Set API key
-key = os.getenv("WEATHER_API")
-
-
-def get_location():
-    "Verify location by calling API on that location and check response code"
-
-    while True:
-        try:
-            # Get location from user:
-            location = input("Please enter a location: ").strip().capitalize()
-
-            # Store location in url
-            location_string = f"http://api.weatherapi.com/v1/current.json?key={key}&q={location}&aqi=no"
-
-            # Call API with location and check response
-            response = requests.get(location_string)
-
-            # If 200, then OK
-            if response.status_code == 200:
-                return location
-
-            # If not 200, then reprompt for location.
-            if response.status_code != 200:
-                print(f"{location} is not a valid location, please try again. ")
-                continue
-
-        # Catch exceptions
-        except Exception as e:
-            print(f"error in getting location: {e}")
-            raise
-
-
-
-def get_temperature(location):
-    "Access the api and return the temperature"
-
-    # Url for calling get_weather API
-    base_url = f"http://api.weatherapi.com/v1/current.json?key={key}&q={location}&aqi=no"
-    
-    try:
-        # Send GET request
-        response = requests.get(base_url)
-
-        # Format response to json for indexing
-        data = response.json()
-
-        # Index into temperature key
-        temperature = data["current"]["temp_c"]
-
-        return temperature
-
-    # Catch exception
-    except Exception:
-        print(f"Error trying to get temperature for location!")
-        raise    
+from data import validate_input, extra_data
+from weather import get_location, get_temperature
 
 
 def main():
@@ -69,10 +9,30 @@ def main():
         location = get_location()
 
         # Return temperature of location
-        temperature = get_temperature(location)
+        temperature, data = get_temperature(location)
 
-        # Print answer
         print(f"The current temperature in {location} is: {temperature} degrees.")
+        print()
+
+        # Validate input
+        status = validate_input()
+
+        if not status:
+            print("Goodbye.")
+            return
+
+        if status:
+            result = extra_data(data)
+        
+        # Index into extra data
+        time = result.get("local_time")
+        condition = result.get("condition")
+        windspeed = result.get("windspeed")
+
+        print(f"The current time in {location} is {time}")
+        print(f"The current condition in {location} is: {condition}")
+        print(f"The windspeed in {location} is currently {windspeed} kph")
+        print()
 
     except Exception as e:
         print(f"Error happened: {e}")
--- data.py
+++ data.py
@@ -1,22 +1,30 @@
-def other_data(input):
+def validate_input():
     "Checks if user is interested in getting more data about requested location."
-    if input == "yes":
-        return True
-    else:
-        print("Not valid input")
-        return False
+    while True:
+        answer = input("Do you want to know more? Type 'yes': ").strip().lower()
+        print()
+        
+        if answer == "yes":
+            return True
+        elif answer == "no":
+            return False
+        else:
+            print("Not valid input. Please type again")
 
 
-def get_more_data(data, location):
+def extra_data(data):
     try:
         local_time = data["location"]["localtime"]
-        print(f"The time at {location} is: {local_time}")
         
         condition = data["current"]["condition"]["text"]
         windspeed = data["current"]["wind_kph"]
-    
-        print(f"The current condition is {condition}, with wind speed at {windspeed} kph.")
 
+        indexed_data = {
+            "local_time": local_time,
+            "condition": condition,
+            "windspeed": windspeed
+        }
+        return indexed_data
     
     except Exception as e:
         print(f"Error trying getting more data from JSON file: {e}")
